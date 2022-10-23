933593c877fe61b585b7e0a19664651400494032
LeventDemir <leventdemir518@gmail.com>
2022-10-23 19:47:39 +0300

    config.fish

---
+++ config.fish
@@ -0,0 +1,50 @@
+function fish_prompt
+  # Cache exit status
+  set -l last_status $status
+
+  # Just calculate these once, to save a few cycles when displaying the prompt
+  if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
+  if not set -q __fish_prompt_char
+    switch (id -u)
+      case 0
+	set -g __fish_prompt_char '#'
+      case '*'
+	set -g __fish_prompt_char 'λ'
+    end
+  end
+
+  # Setup colors
+  set -l normal (set_color normal)
+  set -l red (set_color red)
+  set -l blue (set_color 34E2E2)
+  set -l yellow (set_color yellow)
+  set -l cyan (set_color cyan)
+  set -l red (set_color red)
+  set -l white (set_color white)
+
+  # Configure __fish_git_prompt
+  set -g __fish_git_prompt_char_stateseparator ' '
+  set -g __fish_git_prompt_color white
+  set -g __fish_git_prompt_color_flags red
+  set -g __fish_git_prompt_color_prefix cyan
+  set -g __fish_git_prompt_color_suffix cyan
+  set -g __fish_git_prompt_showdirtystate true
+  set -g __fish_git_prompt_showuntrackedfiles true
+  set -g __fish_git_prompt_showstashstate true
+  set -g __fish_git_prompt_show_informative_status true
+  
+  # Line 1
+  echo -n $red'┌─['$red$USER$yellow'@'$blue$__fish_prompt_hostname$red']'$red'─'$red'['$yellow(prompt_pwd)$red']'
+  __fish_git_prompt "-[git://%s]-"
+  echo
+
+  # Line 2
+  echo -n $red'└──╼ '$yellow$__fish_prompt_char $normal
+end
+
+
+set fish_greeting
+set fish_prompt_pwd_dir_length 100
+set fish_color_command 50FA7B
